97440a8b31096f735ba1f79269ba567359c521bc
Hnungh <hnunghnoy1@gmail.com>
2025-12-06 20:02:56 +0700

    Create data_utility.js

---
+++ sdk/data_utility.js
@@ -0,0 +1,61 @@
+// data_utility.js
+
+/**
+ * โครงสร้างข้อมูลสินค้าจำลอง 
+ * (ใช้สำหรับ Mock Data Source แทนการเรียก API จริง)
+ */
+const MOCK_PRODUCTS_DATA = [
+    {
+        id: 'oversized-tee-black',
+        name: 'Classic Black Oversized Tee',
+        price: 790,
+        description: 'เสื้อยืดโอเวอร์ไซส์สีดำคลาสสิก ผลิตจากผ้าคอตตอน 100% ทรงหลวมสบายสไตล์สตรีท',
+        qty: 1,
+        size: 'M', 
+        color: 'Black',
+        image: 'https://hnungh.github.io/mcl/img/oversized-tee-black.jpg', // ตัวอย่าง URL จริง
+        category: 'OVERSIZED TEE',
+        available_sizes: ['XS', 'S', 'M', 'L', 'XL'],
+        available_colors: [{ name: 'Black', hex: '#000000' }, { name: 'White', hex: '#FFFFFF' }]
+    },
+    {
+        id: 'cargo-pants-olive',
+        name: 'Military Olive Cargo Pants',
+        price: 1590,
+        description: 'กางเกงคาร์โก้สีเขียวมะกอก ดีไซน์สไตล์ทหาร Y2K กระเป๋าเยอะ เนื้อผ้าทนทานเหมาะสำหรับกิจกรรมในเมือง',
+        qty: 1,
+        size: 'L',
+        color: 'Olive',
+        image: 'https://hnungh.github.io/mcl/img/cargo-pants-olive.jpg',
+        category: 'CARGO PANTS',
+        available_sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+        available_colors: [{ name: 'Olive', hex: '#808000' }, { name: 'Black', hex: '#000000' }]
+    },
+    {
+        id: 'hoodie-grey',
+        name: 'Stone Grey Pullover Hoodie',
+        price: 1290,
+        description: 'เสื้อฮู้ดดี้สีเทาหิน ทรงคลาสสิก ผ้านุ่มพิเศษ ให้ความอบอุ่นและสวมใส่สบายในทุกสภาพอากาศ',
+        qty: 1,
+        size: 'XL',
+        color: 'Grey',
+        image: 'https://hnungh.github.io/mcl/img/hoodie-grey.jpg',
+        category: 'PULLOVER HOODIE',
+        available_sizes: ['S', 'M', 'L', 'XL'],
+        available_colors: [{ name: 'Grey', hex: '#808080' }, { name: 'Navy', hex: '#000080' }]
+    },
+    {
+        id: 'sunglasses-retro',
+        name: 'Retro Square Sunglasses',
+        price: 890,
+        description: 'แว่นตากันแดดทรงสี่เหลี่ยมสไตล์ย้อนยุค Y2K เฟรมหนาและเลนส์คุณภาพดี ป้องกัน UV ได้ 100%',
+        qty: 1,
+        size: 'One Size',
+        color: 'Black',
+        image: 'https://hnungh.github.io/mcl/img/sunglasses-retro.jpg',
+        category: 'SUNGLASSES',
+        available_sizes: ['One Size'],
+        available_colors: [{ name: 'Black', hex: '#000000' }, { name: 'Tortoise', hex: '#824510' }]
+    }
+    // เพิ่มสินค้าอื่นๆ จาก MOCK_PRODUCTS_DATA เดิมของคุณที่นี่
+];
